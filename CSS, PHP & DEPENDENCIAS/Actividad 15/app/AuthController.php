9215cf3908a51837a0a215809bc4d739fa661808
RobertoLD <130101004+RobertoLD@users.noreply.github.com>
2024-10-23 14:50:36 -0700

    Actividad 15

--- CSS, PHP & DEPENDENCIAS/Actividad 15/app/AuthController.php
+++ CSS, PHP & DEPENDENCIAS/Actividad 15/app/AuthController.php
@@ -1,40 +1,39 @@
 <?php
 
-if(isset($_POST["accion"])){
+if (isset($_POST["accion"])) {
     $email = $_POST["email"];
     $password = $_POST["password"];
 
     $authController = new AuthController();
-    $authController($email, $password)
+    $authController->login($email, $password);
 }
 
-public class AuthController(){
-    public function login($email, $password){
+class AuthController {
+    public function login($email, $password) {
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-          CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login?email=robertoall_20%40alu.uabcs.mx&password=123456815',
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => '',
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 0,
-          CURLOPT_FOLLOWLOCATION => true,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => 'POST',
-          CURLOPT_POSTFIELDS => array('email' => 'robertoall_20@alu.uabcs.mx','password' => '123456815'),
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login?email=' . urlencode($email) . '&password=' . urlencode($password),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array('email' => $email, 'password' => $password),
         ));
         
         $response = curl_exec($curl);
-        
         curl_close($curl);
-        echo $response;
 
-        $response=json_decode($response);
+        $response = json_decode($response);
 
-        if(isset($response->message))
-
-        header("Location: ../Home.html")
+        if (isset($response->message)) {
+            echo "<script>alert('Error: " . $response->message . "'); window.location.href='./login.html';</script>";
+        } else {
+            header("Location: ../Home.html");
+            exit();
+        }
     }
 }
-
-
